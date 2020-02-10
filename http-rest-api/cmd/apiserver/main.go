7e58ba0e374f313a879bf736e4c379a1df03cb20
Makary <makary.b@gmail.com>
2020-02-10 12:55:55 +0300

    Добавляет поддержку конфигов, логирование, HTTP-сервер с одним роутом
    HTTP-сервер с одним роутом покрыт тестами

---
+++ http-rest-api/cmd/apiserver/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"github.com/BurntSushi/toml"
+	"github.com/makaryb/nashare/http-rest-api/internal/app/apiserver"
+	"log"
+)
+
+// путь к файлу, который будет являться нашим конфигом, будет задаваться в кач-ве флага при запуске бинарника
+var (
+	configPath string
+)
+
+func init() {
+	// что хотим парсить
+	flag.StringVar(&configPath,
+		"config-path",
+		"configs/apiserver.toml",
+		"path to config file")
+}
+
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	// распарсить наш файл и записать все, что нужно в переменную config
+	_, err := toml.DecodeFile(configPath, config)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := apiserver.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
