05b5c7f1396b5b9b8d7d83c3f5970a976a2a1e37
Makary <makary.b@gmail.com>
2020-02-12 23:36:10 +0300

    Добавляет возможность сервера принимать зависимости извне, упрощает его
    Добавляет дополнительную реализацию хранилища для тестов

---
+++ http-rest-api/internal/app/store/repository.go
@@ -0,0 +1,9 @@
+package store
+
+import "github.com/makaryb/nashare/http-rest-api/internal/app/model"
+
+// UserRepository ...
+type UserRepository interface {
+	Create(user *model.User) error
+	FindByEmail(string) (*model.User, error)
+}
---
+++ http-rest-api/internal/app/store/sqlstore/store_test.go
@@ -0,0 +1,21 @@
+package sqlstore_test
+
+import (
+	"os"
+	"testing"
+)
+
+var (
+	databaseURL string
+)
+
+func TestMain(m *testing.M) {
+	databaseURL = os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		databaseURL = "host=localhost dbname=go_restapi_test sslmode=disable password=rony80"
+	}
+
+	os.Exit(m.Run())
+}
+
+
